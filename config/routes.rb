ae221d856f947a970132be60f678abd4294db5e3
EC2 Default User <ec2-user@ip-172-31-91-214.ec2.internal>
2019-07-22 03:03:48 +0000

    Submit the the homework for lesson 14

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   resources :memos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts do
-    resources :comments, only: [:create,:destroy]
+    resources :comments, only: [:create, :destroy]
   end
+  resources :users
+  resources :sessions
   root 'posts#index'
 end
